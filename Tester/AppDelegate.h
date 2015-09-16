5a5d2ffe2105412f6c8905c75e09a17dbb471aa4
MarkD'angelo <markdangelo1@markscomputer.local>
2015-09-16 14:34:16 -0700

    Initial Commit

---
+++ Tester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tester
+//
+//  Created by MarkD'angelo on 9/16/15.
+//  Copyright (c) 2015 MarkD'angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tester/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Tester
+//
+//  Created by MarkD'angelo on 9/16/15.
+//  Copyright (c) 2015 MarkD'angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
